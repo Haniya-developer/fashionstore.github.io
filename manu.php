d80a7667750bb2b9cd5f681782032147347941e0
Haniya-developer <71374697+Haniya-developer@users.noreply.github.com>
2020-09-16 01:27:52 -0700

    my first live web
     nice

---
+++ manu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="fashion.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<section class="header">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="#"><img src="image/fashion-logo2.jpg"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars" aria-hidden="true"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            
+            <li class="nav-item">
+              <a class="nav-link" href="fashion.html">HOME</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="studio.html">STUDIO</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="try.html">CONTACT</a>
+            </li>
+            
+          </ul>
+
+          <ul class="right-menu ml-auto">
+            <li><i class="fa fa-facebook-official" aria-hidden="true"></i></li>
+            <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
+            <li><i class="fa fa-pinterest-p" aria-hidden="true"></i></li>
+            <li>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="SEARCH">
+                <div class="input-group-append">
+                  <span class="input-group-text"><i class="fa fa-search" aria-hidden="true"></i></span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+
+      
+</body>
+</html>
+
